b3c3a520b825283a92e459e94a63c898fe114ef3
gcwelborn <gcruzwelborn@gmail.com>
2016-01-25 19:28:17 -0800

    gotta stop committing the config files, ugh

--- server/twilioController.js
+++ server/twilioController.js
@@ -1,8 +1,9 @@
 // twilio creds
-var Config = require('./../config.js');
+//var Config = require('./../config.js') || null;
 // require twilio module and create a REST client
-var Twilio = require('twilio')(Config.ACCOUNTSID, Config.AUTHTOKEN);
-var image = require('./../images/images.js');
+var Twilio = require('twilio')(process.env.ACCOUNTSID || Config.ACCOUNTSID,
+  process.env.AUTHTOKEN || Config.AUTHTOKEN);
+//var image = require('./../images/images.js');
 var Images = require('./popkeyController');
 
 var Texting = {};
@@ -12,8 +13,8 @@ function sendingText(req, res){
   Images.get_GIF_images(req.body.topic, req.body.pictureAmount, function(err, gif_urls) {
     for (var i = 0; i < gif_urls.length; i++) {
       Twilio.messages.create({
-        to: '+1' + req.body.phone,
-        from: Config.from,
+        to: process.env.phone ||'+1' + req.body.phone,
+        from:process.env.from || Config.from,
         body: 'testiloveing',
         mediaUrl: gif_urls[i]
       }, function(err, message) {
