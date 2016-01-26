46d9f99f13ffd038853e9b10d730424d82af73a1
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-01-25 16:03:58 -0800

    updates react

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname, './../index.html'));
 });
 
-app.post("/", payment.createCharge, Texting.sendingText);
+app.post("/stripe", payment.createCharge, Texting.sendingText);
 
 app.listen(8080, function(){
   console.log('Server is lisening on port 8080');
